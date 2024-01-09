7904a91df3a56bedfb68516be9ccc0ec1f4cf2be
someskyfly <156029858+someskyfly@users.noreply.github.com>
2024-01-09 03:25:25 +0300

    Add files via upload

---
+++ js/cursovaya.js
@@ -0,0 +1,251 @@
+function fisDiv() { //все функции устанавливают активные кнопки. которые переводят на другую таблицу.
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "block";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function secDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "block";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function thiDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "block";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function fourDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "block";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function fifDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "block";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function fifDopDiv() {
+  var firstDiv = document.getElementById("firstDiv"); //
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "block";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function sixDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "block";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function sevDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "block";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function eigDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "block";
+    ninethDiv.style.display = "none";
+}
+
+function eigDopDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "block";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
+
+function ninDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "none";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "block";
+}
+
+function ninDopDiv() {
+  var firstDiv = document.getElementById("firstDiv");
+  var secondDiv = document.getElementById("secondDiv");
+  var thirdDiv = document.getElementById("thirdDiv");
+  var fourthDiv = document.getElementById("fourthDiv");
+  var fifthDiv = document.getElementById("fifthDiv");
+  var sixthDiv = document.getElementById("sixthDiv");
+  var seventhDiv = document.getElementById("seventhDiv");
+  var eigththDiv = document.getElementById("eigththDiv");
+  var ninethDiv = document.getElementById("ninethDiv");
+    firstDiv.style.display = "none";
+    secondDiv.style.display = "none";
+    thirdDiv.style.display = "none";
+    fourthDiv.style.display = "none";
+    fifthDiv.style.display = "none";
+    sixthDiv.style.display = "none";
+    seventhDiv.style.display = "block";
+    eigththDiv.style.display = "none";
+    ninethDiv.style.display = "none";
+}
